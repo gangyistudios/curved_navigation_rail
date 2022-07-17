3c776b20cd43a2975658ccff9be04eeb4ab71bb7
Nissan Silvia <200sxmeda@gmail.com>
2022-07-18 00:43:13 +1000

    Add and update examples

---
+++ example/lib/minimal_example.dart
@@ -0,0 +1,64 @@
+import 'package:curved_navigation_rail/curved_navigation_rail.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CurvedNavigationRailExamplePage(),
+    );
+  }
+}
+
+class CurvedNavigationRailExamplePage extends StatefulWidget {
+  const CurvedNavigationRailExamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<CurvedNavigationRailExamplePage> createState() =>
+      _CurvedNavigationRailExamplePageState();
+}
+
+class _CurvedNavigationRailExamplePageState
+    extends State<CurvedNavigationRailExamplePage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<NavigationRailDestination> destinations = [
+      NavigationRailDestination(
+          icon: Icon(Icons.person), label: Text('Profile')),
+      NavigationRailDestination(icon: Icon(Icons.home), label: Text('Home')),
+      NavigationRailDestination(
+          icon: Icon(Icons.settings), label: Text('Settings')),
+    ];
+
+    return Scaffold(
+        body: Row(
+      children: [
+        CurvedNavigationRail(
+          destinations: destinations,
+          onDestinationSelected: (index) =>
+              setState(() => _selectedIndex = index),
+        ),
+        MyContent(value: _selectedIndex),
+      ],
+    ));
+  }
+}
+
+class MyContent extends StatelessWidget {
+  const MyContent({Key? key, required this.value}) : super(key: key);
+
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Center(child: Text('This is Page $value')));
+  }
+}
