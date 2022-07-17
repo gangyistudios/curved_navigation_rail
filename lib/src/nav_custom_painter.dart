7c93adc79c05a3ba158611530e22b504e209acdc
Nissan Silvia <200sxmeda@gmail.com>
2022-07-17 23:05:14 +1000

    Clean up NavCustomPainter implementation

--- lib/src/nav_custom_painter.dart
+++ lib/src/nav_custom_painter.dart
@@ -13,7 +13,7 @@ class NavCustomPainter extends CustomPainter {
     final span = 1.0 / itemsLength;
     s = 0.2;
     double l = startingLoc + (span - s) / 2;
-    loc = false ? 0.8 - l : l;
+    loc = textDirection == TextDirection.rtl ? 0.8 - l : l;
   }
 
   @override
@@ -22,69 +22,62 @@ class NavCustomPainter extends CustomPainter {
       ..color = color
       ..style = PaintingStyle.fill;
 
-    //// SkETCHPAD STUFF
-    // Move pointer to TOP RIGHT, X positive axis right , Y positive axis downwards
-    var l0x = size.width;
-    var l0y = 0.0;
-    // Draw a line (length = 10% of the height) upwards, which will then begin the cubic curve
-    var l1x = l0x;
-    var l1y = (loc - 0.1) * size.height;
-    // A point indented slightly inwards (to the left), and downwards
-    var c1x1 = size.width - size.width * 0.05;
-    var c1y1 = (loc + s * 0.20) * size.height;
-    // A point at y=0 and x = width -
-    var c1x2 = size.width - size.width * 0.6; //
-    var c1y2 = loc * size.height; // y changes as loc traverses downwards
-    // A point directly to the left of the first rail item
-    var c1x3 = c1x2;
-    var c1y3 = (loc + s * 0.5) * size.height;
+    final double w = size.width;
+    final double h = size.height;
 
-    //
-    var c2x1 = c1x2;
-    var c2y1 = (loc + s) * size.height;
-    //
-    var c2x2 = c1x1;
-    var c2y2 = (loc + s - s * 0.2) * size.height;
-    //
-    var c2x3 = size.width;
-    var c2y3 = (loc + s + 0.1) * size.height;
-
-    //
-    var l2x = size.width;
-    var l2y = size.height;
-
-    var l3x = 0.0;
-    var l3y = size.height;
-
-    var l4x = 00.0;
-    var l4y = 0.0;
+    List<Point> p = [
+      Point(w, 0),
+      Point(w, (loc - 0.1) * h),
+      Point(w * 0.95, (loc + s * 0.20) * h),
+      Point(w * 0.4, loc * h),
+      Point(w * 0.4, (loc + s * 0.5) * h),
+      Point(w * 0.4, (loc + s) * h),
+      Point(w * 0.95, (loc + s - s * 0.2) * h),
+      Point(w, (loc + s + 0.1) * h),
+      Point(w, h),
+      Point(0, h),
+      Point(0, 0),
+    ];
 
     final path = Path()
-      ..moveTo(l0x, l0y) // top left
-      ..lineTo(l1x, l1y) // top left, then left a bit
-      ..cubicTo(c1x1, c1y1, c1x2, c1y2, c1x3, c1y3)
-      ..cubicTo(c2x1, c2y1, c2x2, c2y2, c2x3, c2y3)
-      ..lineTo(l2x, l2y) // top right corner
-      ..lineTo(l3x, l3y) // bottom right corner
-      ..lineTo(l4x, l4y) // bottom left corner
+      ..moveTo(p[0].x, p[0].y)
+      ..lineTo(p[1].x, p[1].y)
+      ..cubicTo(p[2].x, p[2].y, p[3].x, p[3].y, p[4].x, p[4].y)
+      ..cubicTo(p[5].x, p[5].y, p[6].x, p[6].y, p[7].x, p[7].y)
+      ..lineTo(p[8].x, p[8].y)
+      ..lineTo(p[9].x, p[9].y)
+      ..lineTo(p[10].x, p[10].y)
       ..close();
     canvas.drawPath(path, paint);
 
-    // var xpoints = [l0x, l1x, c1x1, c1x2, c1x3, c2x1, c2x2, c2x3, l2x, l3x, l4x];
-    // var ypoints = [l0y, l1y, c1y1, c1y2, c1y3, c2y1, c2y2, c2y3, l2y, l3y, l4y];
-    // // Draw points
-    // var paint1 = Paint()
-    //   ..color = Colors.brown
-    //   ..strokeWidth = 20;
-    // var points = <Offset>[];
-    // for (int i = 0; i < xpoints.length; i++) {
-    //   points.add(Offset(xpoints[i], ypoints[i]));
-    // }
-    // canvas.drawPoints(PointMode.points, points, paint1);
+    // visualisePoints(canvas, p);
+  }
+
+  void visualisePoints(Canvas canvas, List<Point> points) {
+    var paint = Paint()
+      ..color = Colors.brown
+      ..strokeWidth = 20;
+    canvas.drawPoints(
+        PointMode.points, points.map((e) => Offset(e.x, e.y)).toList(), paint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return this != oldDelegate;
+  }
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  Point(
+    this.x,
+    this.y,
+  );
+
+  @override
+  String toString() {
+    return ('($x, $y)');
   }
 }
